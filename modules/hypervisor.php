6d833702892747f6d8acd6bfb86111e112ebc4f8
Jiab77 <jonathan.barda@gmail.com>
2020-03-20 23:21:18 +0100

    Added some content

--- modules/hypervisor.php
+++ modules/hypervisor.php
@@ -3,6 +3,9 @@
 <div class="row">
 	<div class="col s12">
 		<h1>Hypervisor</h1>
+
+		<?php if (!isset($_GET['do']) || (isset($_GET['do']) && empty($_GET['do']))): ?>
+
 		<div class="row">
 			<div class="col s6 m4 l3">
 				<div class="card-panel hoverable">
@@ -37,6 +40,26 @@
 			<div class="col s6 m4 l3">
 				<div class="card-panel hoverable">
 					<p class="flow-text center-align">
+						<a href="<?php echo '?module=' . $module . '&do=create&type=img'; ?>">
+							<i class="material-icons">storage</i>
+							<br><span class="truncate">Create Images</span>
+						</a>
+					</p>
+				</div>
+			</div>
+			<div class="col s6 m4 l3">
+				<div class="card-panel hoverable">
+					<p class="flow-text center-align">
+						<a href="<?php echo '?module=' . $module . '&do=create&type=sto'; ?>">
+							<i class="material-icons">storage</i>
+							<br><span class="truncate">Create Storage</span>
+						</a>
+					</p>
+				</div>
+			</div>
+			<div class="col s6 m4 l3">
+				<div class="card-panel hoverable">
+					<p class="flow-text center-align">
 						<a href="#modal-upload" class="modal-trigger">
 							<i class="material-icons">storage</i>
 							<br><span class="truncate">Upload Images</span>
@@ -45,9 +68,75 @@
 				</div>
 			</div>
 		</div>
-		<!-- <div class="row">
+
+		<?php endif; ?>
+		<?php
+		if (isset($_GET['do']) && !empty($_GET['do'])) {
+			switch ($_GET['do']) {
+				case 'create':
+					if (isset($_GET['type']) && !empty($_GET['type'])) {
+						switch ($_GET['type']) {
+							case 'img':
+								$create_type = ' Images';
+								$create_cmd  = 'virt-builder --help';
+								$virsh = false;
+								break;
+
+							case 'net':
+								$create_type = ' Network';
+								$create_cmd  = 'help net-create';
+								$virsh = true;
+								break;
+
+							case 'sto':
+								$create_type = ' Storage';
+								$create_cmd  = 'help pool-create-as';
+								$virsh = true;
+								break;
+
+							case 'vm':
+								$create_type = ' VM';
+								// $create_cmd  = 'help create';
+								$create_cmd  = 'virt-install --help';
+								$virsh = false;
+								break;
+							
+							default:
+								$create_type = '';
+								$create_cmd  = '';
+								$virsh = false;
+								break;
+						}
+					}
+					?>
+
+		<div class="row">
 			<div class="col s12">
-				<h5>Parsed data</h5>
+				<h3><?php echo 'Create' . $create_type; ?></h3>
+				<!-- <h5>Parsed data</h5> -->
+				<pre style="height: 300px;"><?php
+				if (isset($create_cmd) && !empty($create_cmd)) {
+					if ($virsh === true) {
+						echo htmlentities($libVirt->virsh_shell_exec($create_cmd));
+					}
+					else {
+						echo htmlentities($libVirt->shell_exec_cmd($create_cmd));
+					}
+				}
+				?></pre>
+			</div>
+		</div>
+
+					<?php
+					break;
+
+				case 'view':
+					?>
+
+		<div class="row">
+			<div class="col s12">
+				<h3>System Info</h3>
+				<h6>Parsed data</h6>
 				<pre style="height: 300px;"><?php
 				print_r($libVirtXML->xml2json($libVirt->virsh_shell_exec('sysinfo'), false, true));
 				?></pre>
@@ -56,7 +145,17 @@
 				echo htmlentities($libVirt->virsh_shell_exec('sysinfo'));
 				?></pre>
 			</div>
-		</div> -->
+		</div>
+
+					<?php
+					break;
+				
+				default:
+					# code...
+					break;
+			}
+		}
+		?>
 	</div>
 </div>
 
